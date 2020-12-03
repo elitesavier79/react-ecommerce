4a3deb5face999e8525d00eae963c6fd37750118
elitesavier79 <elitesavier79@gmail.com>
2020-12-03 15:00:14 +0700

    implementasi tampilan layout dan header

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,35 @@
+import Layout from '@common/components/Layout';
+
+
 const HomePages = ({ products }) => {
   const { list } = products
   
   return (
-      <div>
-          {
-            list.map(list => {
-              return (
-                  <p>{list.name}</p>
+    <Layout>
+       <div>
+            {
+              list.map((list, index) => {
+                return (
 
-                )
-            })
-          }
-      </div>
+                    <p key={index}>{list.name}</p>
+
+                  )
+              })
+            }
+        </div>
+     </Layout>
 
     )
 }
 
 export async function getStaticProps() {
-  const res = await fetch('http://localhost:3000/api/products');
+  const res = await fetch(`http://localhost:3000/api/products`);
   const products = await res.json();
 
   return {
-    props:{
-      products
-    }
+    props: {
+      products,
+    },
   }
 }
 
