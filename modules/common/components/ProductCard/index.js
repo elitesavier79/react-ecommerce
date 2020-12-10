7413b85409bbc14bd5378482e16e728cdd4a4e45
elitesavier79 <elitesavier79@gmail.com>
2020-12-10 22:47:20 +0700

    membuat product rating

--- modules/common/components/ProductCard/index.js
+++ modules/common/components/ProductCard/index.js
@@ -10,6 +10,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
 import PromoLabel from '@common/components/PromoLabel';
+import ProductRating from '@common/components/ProductRating';
 
 const useStyles = makeStyles({
 	card:{
@@ -59,6 +60,7 @@ const ProductCard = ({ img, title, promoLabel, price, rating, sold, productID })
 				  		<Typography variant="overline" className={classes.price}>
 				  			{price}
 				  		</Typography>
+				  		<ProductRating rating={rating} sold={sold} />
 				  	</Grid>
 				  </CardContent>
 			</div>
